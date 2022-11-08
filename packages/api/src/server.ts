3e13cf7ec8b67c80e12f0a8e178186680e4f06ad
Marco Valtas <mvaltas@thoughtworks.com>
2022-11-07 19:28:39 -0500

    Spike on API documentation generator for CCF

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -12,6 +12,7 @@ import helmet from 'helmet'
 import { createRouter } from './api'
 import auth from './auth'
 import { Logger } from '@cloud-carbon-footprint/common'
+import swaggerUi from 'swagger-ui-express'
 
 const port = process.env.PORT || 4000
 const httpApp = express()
@@ -24,6 +25,18 @@ if (process.env.NODE_ENV === 'production') {
 httpApp.use(helmet())
 
 httpApp.use('/api', createRouter())
+
+httpApp.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: '/swagger.json',
+    },
+  }),
+)
+// to serve the swagger.json file
+httpApp.use(express.static(process.cwd() + '/public'))
 
 httpApp.listen(port, () =>
   serverLogger.info(
